1ffe4c9263df44631a40c6b2fdf81d3271f2a344
Malcom Green <malcomeqgreen@gmail.com>
2023-08-02 15:40:58 -0400

    Session change

--- lib/session.ts
+++ lib/session.ts
@@ -1,8 +1,9 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from './auth';
+import { NextRequest } from 'next/server';
 
-export async function getCurrentUser() {
-    const session = await getServerSession(authOptions(undefined))
+export async function getCurrentUser(req: NextRequest) {
+    const session = await getServerSession(authOptions(req))
 
     return session?.user;
 }
