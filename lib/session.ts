b1b0cdce462976e9a7ac0abb86fbbf13ef891f94
Malcom Green <malcomeqgreen@gmail.com>
2023-08-02 22:15:48 -0400

    Ignore undefined

--- lib/session.ts
+++ lib/session.ts
@@ -3,6 +3,7 @@ import { authOptions } from './auth';
 import { NextRequest } from 'next/server';
 
 export async function getCurrentUser() {
+    // @ts-expect-error
     const session = await getServerSession(authOptions(undefined))
 
     return session?.user;
