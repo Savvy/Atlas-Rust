a4718a9f1b085ed4120f568c1f5db02ae9a99f19
Malcom Green <malcomeqgreen@gmail.com>
2023-09-07 11:11:37 -0400

    added option to enable / disable cart

--- data/navigation.ts
+++ data/navigation.ts
@@ -2,30 +2,37 @@ export const mainNav = [
     {
         title: "Home",
         href: "/",
+        disabled: false,
+        showCart: false,
     },
     {
         title: "Servers",
         href: "/servers",
         disabled: false,
+        showCart: false,
     },
     {
         title: "Leaderboard",
         href: "/leaderboard",
         disabled: false,
+        showCart: false,
     },
     {
         title: "Clans",
         href: "/clans",
         disabled: true,
+        showCart: false,
     },
     {
         title: "Maps",
         href: "/maps",
         disabled: true,
+        showCart: false,
     },
     {
         title: "Store",
         href: "/store",
-        disabled: true,
+        disabled: false,
+        showCart: true,
     },
 ]
