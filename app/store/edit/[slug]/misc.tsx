edeaec34ecda326bca7ed21eb43a85d442aa4d19
Malcom Green <malcomeqgreen@gmail.com>
2023-11-19 19:36:45 -0500

    Fixed labels

--- app/store/edit/[slug]/misc.tsx
+++ app/store/edit/[slug]/misc.tsx
@@ -26,11 +26,76 @@ type MiscProps = {
     setColorHex: (value: string) => void
 }
 
+const kitLabels = [
+    '12h',
+    '10h',
+    '8h',
+    '6h',
+    '4h',
+]
+
+const tpLabels = [
+    '25S',
+    '20S',
+    '15S',
+    '10S',
+    '5S',
+]
+
+const homesLabels = [
+    '3',
+    '5',
+    '10',
+    '15',
+    '20',
+]
+
+const colorLabels = [
+    'Blue',
+    'Red',
+    'Yellow',
+    'Pink',
+    'Custom',
+]
+
 export default function Miscellaneous(props: MiscProps) {
+
+    const THUMB_SIZE = 12;
+
+    function calcStepMarkOffset(index: number, maxIndex: number) {
+        const percent = convertValueToPercentage(index, 0, maxIndex);
+        const thumbInBoundsOffset = getThumbInBoundsOffset(THUMB_SIZE, percent, 1);
+        return `calc(${percent}% + ${thumbInBoundsOffset}px)`;
+    }
+
+    const clamp = (val: number, min: number, max: number) => Math.min(Math.max(val, min), max)
+
+    function convertValueToPercentage(value: number, min: number, max: number) {
+        const maxSteps = max - min;
+        const percentPerStep = 100 / maxSteps;
+        const percentage = percentPerStep * (value - min);
+        return clamp(percentage, 0, 100);
+    }
+
+    function getThumbInBoundsOffset(width: number, left: number, direction: number) {
+        const halfWidth = width / 2;
+        const halfPercent = 50;
+        const offset = linearScale([0, halfPercent], [0, halfWidth]);
+        return (halfWidth - offset(left) * direction) * direction;
+    }
+
+    function linearScale(input: readonly [number, number], output: readonly [number, number]) {
+        return (value: number) => {
+            if (input[0] === input[1] || output[0] === output[1]) return output[0];
+            const ratio = (output[1] - output[0]) / (input[1] - input[0]);
+            return output[0] + ratio * (value - input[0]);
+        };
+    }
+
     return (
-        <div className="w-full grid grid-cols-12 gap-4">
+        <div className="w-full grid grid-cols-12 gap-6">
             <div className="col-span-6 flex flex-col gap-2 justify-between"> {/* left side */}
-                <div className="flex flex-col gap-2">
+                <div className="relative flex flex-col gap-2">
                     <Label htmlFor="terms" className="font-rajdhani text-muted text-lg">Kit Cooldown</Label>
                     <Slider
                         id="kitCooldown"
@@ -42,15 +107,17 @@ export default function Miscellaneous(props: MiscProps) {
                             props.setKitCooldown(values[0])
                         }}
                     />
-                    <div className="flex justify-between font-rajdhani text-muted font-semibold select-none">
-                        <span>12h</span>
-                        <span>10h</span>
-                        <span>8h</span>
-                        <span>6h</span>
-                        <span>4h</span>
+                    <div className="mb-8 flex justify-between font-rajdhani text-muted font-semibold select-none">
+                        {kitLabels.map((label: string, index) => (
+                            <div
+                                key={index}
+                                className={cn("absolute", "-translate-x-1/2")}
+                                style={{ ["left"]: calcStepMarkOffset(index, kitLabels.length - 1) }}
+                            >{label}</div>
+                        ))}
                     </div>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="relative flex flex-col gap-2">
                     <Label htmlFor="terms" className="font-rajdhani text-muted text-lg">Teleportation Cooldown</Label>
                     <Slider
                         max={4}
@@ -61,15 +128,17 @@ export default function Miscellaneous(props: MiscProps) {
                             props.setTeleportCooldown(values[0])
                         }}
                     />
-                    <div className="flex justify-between font-rajdhani text-muted font-semibold select-none">
-                        <span>25S</span>
-                        <span>20S</span>
-                        <span>15S</span>
-                        <span>10S</span>
-                        <span>5S</span>
+                    <div className="mb-8 flex justify-between font-rajdhani text-muted font-semibold select-none">
+                        {tpLabels.map((label: string, index) => (
+                            <div
+                                key={index}
+                                className={cn("absolute", "-translate-x-1/2")}
+                                style={{ ["left"]: calcStepMarkOffset(index, tpLabels.length - 1) }}
+                            >{label}</div>
+                        ))}
                     </div>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="relative flex flex-col gap-2">
                     <Label htmlFor="terms" className="font-rajdhani text-muted text-lg">Amount of Homes</Label>
                     <Slider
                         max={4}
@@ -80,15 +149,17 @@ export default function Miscellaneous(props: MiscProps) {
                             props.setAmountOfHomes(values[0])
                         }}
                     />
-                    <div className="flex justify-between font-rajdhani text-muted font-semibold select-none">
-                        <span>3</span>
-                        <span>5</span>
-                        <span>10</span>
-                        <span>15</span>
-                        <span>20</span>
+                    <div className="mb-8 flex justify-between font-rajdhani text-muted font-semibold select-none">
+                        {homesLabels.map((label: string, index) => (
+                            <div
+                                key={index}
+                                className={cn("absolute", "-translate-x-1/2")}
+                                style={{ ["left"]: calcStepMarkOffset(index, homesLabels.length - 1) }}
+                            >{label}</div>
+                        ))}
                     </div>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="relative flex flex-col gap-2">
                     <Label htmlFor="terms" className="font-rajdhani text-muted text-lg">Colored Name</Label>
                     <Slider
                         max={4}
@@ -99,12 +170,14 @@ export default function Miscellaneous(props: MiscProps) {
                             props.setColoredName(values[0])
                         }}
                     />
-                    <div className="flex justify-between font-rajdhani text-muted font-semibold select-none">
-                        <span>Blue</span>
-                        <span>Red</span>
-                        <span>Yellow</span>
-                        <span>Pink</span>
-                        <span>Custom</span>
+                    <div className="mb-8 flex justify-between font-rajdhani text-muted font-semibold select-none">
+                        {colorLabels.map((label: string, index) => (
+                            <div
+                                key={index}
+                                className={cn("absolute", "-translate-x-1/2")}
+                                style={{ ["left"]: calcStepMarkOffset(index, colorLabels.length - 1) }}
+                            >{label}</div>
+                        ))}
                     </div>
                 </div>
             </div>
